b28b79586dbd977ed0a84a632bfd5dd57e19877e
Sunraj751 <Sunraj751@gmail.com>
2022-05-26 23:30:08 -0400

    edited

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ window.addEventListener('load',()=>{
             lat = position.coords.latitude;
             const api =`https://api.weatherapi.com/v1/current.json?key=117d8f778efe4c5fb53231729222505&q=${lat},${long}`;
             
-            const iconapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=f4b57399c560c2b41f1438adb75e30ff`
+            const iconapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=f4b57399c560c2b41f1438adb75e30ff`;
             fetch(api)
                 .then(response =>{
                     return response.json()
@@ -40,9 +40,7 @@ window.addEventListener('load',()=>{
                         temperatureDegree.textContent = temp_f;
                         temperatureSpan.textContent ="F";
                     }
-
                    });
-
                 });
             fetch (iconapi)
                 .then(response =>{
